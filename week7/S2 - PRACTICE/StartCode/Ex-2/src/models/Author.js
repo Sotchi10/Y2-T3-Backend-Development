781d57242fc9e6146b3bfe07375d0a4873d0f387
Sotchi <sovanpisothyan@example.com>
2026-07-23 23:33:46 +0700

    remove unrelated weeks

--- week7/S2 - PRACTICE/StartCode/Ex-2/src/models/Author.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
-
-const Author = sequelize.define('Author', {
-  name: DataTypes.STRING,
-  birthYear: DataTypes.INTEGER
-});
-
-export default Author;
--- week7/S2 - PRACTICE/StartCode/Ex-3/src/config/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './Ex-3/src/data/database.sqlite'
-});
-
-export default sequelize;
